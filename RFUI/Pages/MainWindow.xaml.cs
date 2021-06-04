29dea4e8826e5d1e360c6f16d3bdd206b09c1d24
Tunguso4ka <71643624+Tunguso4ka@users.noreply.github.com>
2021-06-04 15:30:50 +0300

    update

--- RFUI/Pages/MainWindow.xaml.cs
+++ RFUI/Pages/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using Forms = System.Windows.Forms;
+using System.Windows.Input;
+using System.ComponentModel;
 
 namespace RFUI
 {
@@ -8,29 +12,103 @@ namespace RFUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        Forms.NotifyIcon notifyIcon;
+        public UpdatePage _UpdatePage;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Создаем NotifyIcon
+            CreateNotifyIcon();
+
+            Pages();
+            Frame0.Navigate(_UpdatePage);
         }
 
+        void Pages()
+        {
+            _UpdatePage = new UpdatePage();
+        }
+
+        public void CheckUpdates()
+        {
+
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //для двиганья окна
+            DragMove();
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //Кнопки
             Button ClickedButton = (Button)sender;
             if ((string)ClickedButton.Tag == "Close")
             {
                 if(Properties.Settings.Default.CanClose == true)
                 {
+                    KillNotifyIcon();
                     this.Close();
                 }
                 else
                 {
-                    this.Hide();
+                    this.WindowState = WindowState.Minimized;
                 }
             }
             else if ((string)ClickedButton.Tag == "Minimize")
             {
-                this.Hide();
+                this.WindowState = WindowState.Minimized;
             }
+        }
+        private void CreateNotifyIcon()
+        {
+            notifyIcon = new Forms.NotifyIcon(new Container());
+
+            Forms.ContextMenuStrip _ContextMenuStrip = new Forms.ContextMenuStrip();
+
+            Forms.ToolStripMenuItem _StripMenuItemAppName = new Forms.ToolStripMenuItem();
+
+            _ContextMenuStrip.Items.AddRange(
+                new Forms.ToolStripMenuItem[]
+                {
+                    _StripMenuItemAppName,
+                    new Forms.ToolStripMenuItem("Exit", null, new EventHandler(ExitClicked))
+                }
+            );
+
+            _StripMenuItemAppName.Text = "RFU Installer";
+            _StripMenuItemAppName.Enabled = false;
+            _StripMenuItemAppName.Image = Properties.Resources.rfulogo0525;
+
+            notifyIcon.Icon = Properties.Resources.rfulogo0525_lcM_icon;
+            notifyIcon.ContextMenuStrip = _ContextMenuStrip;
+            notifyIcon.Text = "RFU Installer";
+            notifyIcon.Visible = true;
+
+            notifyIcon.MouseDown += new Forms.MouseEventHandler(NotifyIconClicked);
+
+        }
+
+        [STAThread]
+        private void NotifyIconClicked(object sender, Forms.MouseEventArgs e)
+        {
+            if (e.Button == Forms.MouseButtons.Left)
+            {
+                this.WindowState = WindowState.Normal;
+                this.ShowInTaskbar = true;
+            }
+        }
+        private void ExitClicked(object sender, EventArgs e)
+        {
+            KillNotifyIcon();
+            Application.Current.Dispatcher.Invoke(Application.Current.Shutdown);
+        }
+
+        public void KillNotifyIcon()
+        {
+            notifyIcon.Dispose();
         }
     }
 }
---
+++ RFUI/Pages/UpdatePage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace RFUI
+{
+    /// <summary>
+    /// Interakční logika pro UpdatePage.xaml
+    /// </summary>
+    public partial class UpdatePage : Page
+    {
+        public UpdatePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Button ClickedButton = (Button)sender;
+            if ((string)ClickedButton.Tag == "Install")
+            {
+                ((MainWindow)Window.GetWindow(this))._ProgressBar.Visibility = Visibility.Visible;
+            }
+            else if ((string)ClickedButton.Tag == "Update")
+            {
+                ((MainWindow)Window.GetWindow(this))._ProgressBar.Visibility = Visibility.Visible;
+            }
+            else if ((string)ClickedButton.Tag == "Info")
+            {
+                if(InfoBorder.Visibility == Visibility.Collapsed)
+                {
+                    ClickedButton.ToolTip = "Hide info";
+                    InfoBorder.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    ClickedButton.ToolTip = "Show info";
+                    InfoBorder.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
