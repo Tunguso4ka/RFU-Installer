95c67c5771162a50cb3757bc72f047e996400265
Tunguso4ka <71643624+Tunguso4ka@users.noreply.github.com>
2021-06-04 12:17:53 +0300

    Přidejte soubory projektu.

---
+++ RFUI/Pages/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RFUI
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button ClickedButton = (Button)sender;
+            if ((string)ClickedButton.Tag == "Close")
+            {
+                if(Properties.Settings.Default.CanClose == true)
+                {
+                    this.Close();
+                }
+                else
+                {
+                    this.Hide();
+                }
+            }
+            else if ((string)ClickedButton.Tag == "Minimize")
+            {
+                this.Hide();
+            }
+        }
+    }
+}
